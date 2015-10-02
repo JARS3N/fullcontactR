55c4885320d099db00e4a008a60bc8dbe6ede98f
JARS3N <JARS3N@users.noreply.github.com>
2015-10-02 16:15:30 -0400

    Update fullcontactR.R

--- R/fullcontactR.R
+++ R/fullcontactR.R
@@ -1,41 +1,36 @@
 ###Fullcontact API
 #requires jsonlite
 
-mail<-function(email,apikey=apikey){
-checkAPI()
+mail<-function(email,apikey){
     fromJSON(paste0(
-    "https://api.fullcontact.com/v2/person.json?email=",
-    email,
-    "&apiKey=",
-    apikey))
-    }
-
-phone<-function(phone,apikey=apikey){
-checkAPI()
-    fromJSON(paste0(
-    "https://api.fullcontact.com/v2/person.json?phone=+",
-    phone,"&apiKey=",apikey))
+        "https://api.fullcontact.com/v2/person.json?email=",
+        email,
+        "&apiKey=",
+        apikey))
 }
 
-twitter<-function(twitter,apikey=apikey){
-checkAPI()
+phone<-function(phone,apikey){
+    fromJSON(paste0(
+        "https://api.fullcontact.com/v2/person.json?phone=+",
+        phone,"&apiKey=",apikey))
+}
+
+twitter<-function(twitter,apikey){
     fromJSON(paste0(
         "https://api.fullcontact.com/v2/person.json?twitter=",
         twitter,"&apiKey=",apikey))
 }
 
-facebook<-function(facebookusername,apikey=apikey){
-checkAPI()
-   out<- fromJSON(paste0(
+facebook<-function(facebookusername,apikey){
+    out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/person.json?facebookUsername=",
         facebookusername,"&apiKey=",apikey))
-   if (is.null(out$message)==FALSE){warning(out$message)}
-   out$Timestamp<-Sys.time()
+    if (is.null(out$message)==FALSE){warning(out$message)}
+    out$Timestamp<-Sys.time()
     out
 }
 
-domain<-function(domain,apikey=apikey){
-checkAPI()
+domain<-function(domain,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/company/lookup.json?domain=",
         domain,"&apiKey=",apikey))
@@ -44,8 +39,7 @@ checkAPI()
     out
 }
 
-deducer<-function(deducer,apikey=apikey){
-checkAPI()
+deducer<-function(deducer,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/name/deducer.json?email=",
         deducer,"&apiKey=",apikey))
@@ -55,8 +49,7 @@ checkAPI()
 
 
 
-similarity<-function(q1,q2,apikey=apikey){
-checkAPI()
+similarity<-function(q1,q2,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/name/similarity.json?q1=",q1,"&q2=",q2,
         "&apiKey=",apikey))
@@ -64,8 +57,7 @@ checkAPI()
     out
 }
 
-locationNormalizer<-function(place,apikey=apikey){
-checkAPI()
+locationNormalizer<-function(place,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/address/locationNormalizer.json?place=",place,
         "&apiKey=",apikey))
@@ -73,8 +65,7 @@ checkAPI()
     out
 }
 
-locationEnrichment<-function(place,apikey=apikey){
-checkAPI()
+locationEnrichment<-function(place,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/address/locationEnrichment.json?place=",place,
         "&apiKey=",apikey))
@@ -85,26 +76,24 @@ checkAPI()
 CacheKey<-function(u){
     saveRDS(u,file.path(system.file(package="fullcontactR"),"fullcontactapikey.rds"))
     if (exists('apikey')==FALSE){
-    apkikey<<-u
+        apkikey<<-u
     }
 }
 
 ### still need to add the folowing
 
 
-disposable<-function(email,apikey=apikey){
-checkAPI()
- out_string<- paste0("https://api.fullcontact.com/v2/email/disposable.json?email=",
-        email,
-        "&apiKey=",apikey)
+disposable<-function(email,apikey){
+    out_string<- paste0("https://api.fullcontact.com/v2/email/disposable.json?email=",
+                        email,
+                        "&apiKey=",apikey)
     out<-fromJSON(URLencode(out_string))
     out$Timestamp<-Sys.time()
     out
 }
 
 #https://api.fullcontact.com/v2/name/stats.json?name=john&apiKey=xxxx
-stats<-function(name,apikey=apikey){
-checkAPI()
+stats<-function(name,apikey){
     out<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/name/stats.json?name=",name,
         "&apiKey=",apikey))
@@ -115,8 +104,7 @@ checkAPI()
 #https://api.fullcontact.com/v2/name/parser.json?q=john%20smith&apiKey=xxxx
 
 
-parser<-function(q,apikey=apikey){
-checkAPI()
+parser<-function(q,apikey){
     out_string<- fromJSON(paste0(
         "https://api.fullcontact.com/v2/name/parser.json?q=",q,
         "&apiKey=",apikey))
@@ -124,5 +112,17 @@ checkAPI()
     out$Timestamp<-Sys.time()
     out
 }
-# shortand to check for APIkey when starting functions,only sends warnings,still attempts
+# shorthand to check for APIkey when starting functions,only sends warnings,still attempts
 checkAPI<-if(exists("apikey")!=T){warning('No apikey detected')}
+
+#period to check usage by 4digityear-2digitmonth
+period<-function(period,apikey){
+    out<-fromJSON(paste0('https://api.fullcontact.com/v2/stats.json?period=',period,'&apiKey=',apikey))
+    out$Timestamp<-Sys.time()
+}
+
+period<-function(period,apikey){
+    out<-fromJSON(paste0('https://api.fullcontact.com/v2/stats.json?period=',period,'&apiKey=',apikey))
+    out$Timestamp<-Sys.time()
+    out
+}
