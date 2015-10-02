e5b848db47c25c04a2f4a79273b3374b1076c166
JARS3N <JARS3N@users.noreply.github.com>
2015-10-02 09:34:53 -0400

    try1.fix4
    needed to the check if file exists first

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname = find.package("fullcontactR"), pkgname = "fullcontactR") {
+if (file.exists(system.file("fullcontactapikey.rds",package="fullcontactR"))==TRUE){
   apikey<<-readRDS(system.file("fullcontactapikey.rds",package="fullcontactR"))  
+  }
 }
