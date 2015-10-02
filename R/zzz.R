3c492176ee0bac6e45e91db443a42cfdb660b99b
JARS3N <JARS3N@users.noreply.github.com>
2015-10-02 10:59:54 -0400

    fixes
    changed message,fixed terrible spelling errors :X. Changed if or else to 2 if statements

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 .onLoad <- function(libname = find.package("fullcontactR"), pkgname = "fullcontactR") {
 if (file.exists(system.file("fullcontactapikey.rds",package="fullcontactR"))==TRUE){
   apikey<<-readRDS(system.file("fullcontactapikey.rds",package="fullcontactR"))  
-  } else{
-  warning("These is no apikey cached on this system and one is required for these functions to work.")
-  warning("Please aquire your apikey from fullcontact.com and run CacheKey() to save the key.")
-  warning("The apikey will be loaded to the global enviroment when this package loads thereafter.")
+  } 
+if (file.exists(system.file("fullcontactapikey.rds",package="fullcontactR"))==FALSE){
+  warning("There is no apikey cached on this system,one is required for these functions to work.Please aquire your apikey from fullcontact.com and run CacheKey() to save the key.The apikey will be loaded to the global enviroment when this package loads thereafter.")
   }
 }
