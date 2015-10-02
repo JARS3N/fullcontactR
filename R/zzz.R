24cd6b50344e1941964e44c6cfd61830474672b5
JARS3N <JARS3N@users.noreply.github.com>
2015-10-02 10:55:19 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 if (file.exists(system.file("fullcontactapikey.rds",package="fullcontactR"))==TRUE){
   apikey<<-readRDS(system.file("fullcontactapikey.rds",package="fullcontactR"))  
   } else{
-  warning("These is no apikey chached on this system and one is required for these functions to work.")
+  warning("These is no apikey cached on this system and one is required for these functions to work.")
   warning("Please aquire your apikey from fullcontact.com and run CacheKey() to save the key.")
   warning("The apikey will be loaded to the global enviroment when this package loads thereafter.")
   }
